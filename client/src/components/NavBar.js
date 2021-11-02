8ba782216727883cbadc150a74a1795d73d6031a
Alex Kulnis <kulnis71@gmail.com>
2021-11-02 09:14:52 +0300

    new eslint

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react';
 import { Context } from '../index';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { observer } from 'mobx-react-lite';
 import { NavLink } from 'react-router-dom';
-import { ADMIN_ROUTE, SHOP_ROUTE, LOGIN_ROUTE } from '../utils/consts';
-import { Button, Container } from 'react-bootstrap';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
+import { Button } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
+import Container from 'react-bootstrap/Container';
 import { useHistory } from 'react-router-dom';
-
 const NavBar = observer(() => {
   const { user } = useContext(Context);
   const history = useHistory();
@@ -18,33 +18,40 @@ const NavBar = observer(() => {
   };
 
   return (
-    <Navbar bg='dark' variant='dark' >
+    <Navbar bg='dark' variant='dark'>
       <Container>
-        <NavLink style={{ color: 'grey' }} to={SHOP_ROUTE}>Купить девайс</NavLink>
-        {user.isAuth ?
-            <Nav>
-              {user.isAuth === 'ADMIN' ?
-                  <Button onClick={() => history.push(ADMIN_ROUTE)}
-                    variant={'outline-light'}
-                    className="ml-4">Админ панель</Button>
-                  :
-                  <div className="ml-4" style={{ color: 'red' }}> Простой раб</div>
-              }
-
-              <Button onClick={() => logOut()}
-                style={{ marginLeft: 20, marginRight: 20 }}
-                variant={'outline-light'}
-              >
-                Выйти
-              </Button>
-            </Nav>
-            :
-            <Nav >
-              <Button style={{ marginLeft: 21 }} variant={'outline-light'} onClick={() => history.push(LOGIN_ROUTE)}>Авторизация</Button>
-            </Nav>
-        }
-      </Container >
-    </Navbar >
+        <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>
+          КупиДевайс
+        </NavLink>
+        {user.isAuth ? (
+          <Nav className='ml-auto' style={{ color: 'white' }}>
+            <Button
+              variant={'outline-light'}
+              onClick={() => history.push(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
+            <Button
+              variant={'outline-light'}
+              onClick={() => logOut()}
+              className='ml-2'
+            >
+              Выйти
+            </Button>
+          </Nav>
+        ) : (
+          <Nav className='ml-auto' style={{ color: 'white' }}>
+            <Button
+              variant={'outline-light'}
+              onClick={() => history.push(LOGIN_ROUTE)}
+            >
+              Авторизация
+            </Button>
+          </Nav>
+        )}
+      </Container>
+    </Navbar>
   );
 });
+
 export default NavBar;
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -36,36 +36,39 @@ const Auth = observer(() => {
 
   return (
     <Container
-      className="d-flex justify-content-center align-items-center"
+      className='d-flex justify-content-center align-items-center'
       style={{ height: window.innerHeight - 54 }}
     >
-      <Card style={{ width: 600 }} className="p-5">
-        <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
-        <Form className="d-flex flex-column">
+      <Card style={{ width: 600 }} className='p-5'>
+        <h2 className='m-auto'>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+        <Form className='d-flex flex-column'>
           <Form.Control
-            className="mt-3"
-            placeholder="Введите ваш email..."
+            className='mt-3'
+            placeholder='Введите ваш email...'
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={e => setEmail(e.target.value)}
           />
           <Form.Control
-            className="mt-3"
-            placeholder="Введите ваш пароль..."
+            className='mt-3'
+            placeholder='Введите ваш пароль...'
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
+            onChange={e => setPassword(e.target.value)}
+            type='password'
           />
-          <Row className="d-flex justify-content-center mt-3 pl-3 pr-3">
-            {isLogin ?
+          <Row className='d-flex justify-content-center mt-3 pl-3 pr-3'>
+            {isLogin ? (
               <div>
-                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                Нет аккаунта?{' '}
+                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
               </div>
-              :
+            ) : (
               <div>
                 Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
               </div>
-            }
-            <Button style={{ width: 220 }} className=" mt-3"
+            )}
+            <Button
+              style={{ width: 220 }}
+              className=' mt-3'
               variant={'outline-success'}
               onClick={click}
             >
